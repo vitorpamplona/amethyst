99f478d89172cfa504a9c2904af7472390421514
Vitor Pamplona <vitor@vitorpamplona.com>
2023-01-19 17:56:44 -0500

    Fixing large usernames breaking the layout

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/UsernameDisplay.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/UsernameDisplay.kt
@@ -3,11 +3,13 @@ package com.vitorpamplona.amethyst.ui.note
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import com.vitorpamplona.amethyst.model.User
 
 @Composable
-fun UsernameDisplay(user: User) {
+fun UsernameDisplay(user: User, weight: Modifier = Modifier) {
     if (user.bestUsername() != null || user.bestDisplayName() != null) {
       if (user.bestDisplayName().isNullOrBlank()) {
         Text(
@@ -22,6 +24,9 @@ fun UsernameDisplay(user: User) {
         Text(
           "@${(user.bestUsername() ?: "")}",
           color = MaterialTheme.colors.onSurface.copy(alpha = 0.32f),
+          maxLines = 1,
+          overflow = TextOverflow.Ellipsis,
+          modifier = weight
         )
       }
     } else {
