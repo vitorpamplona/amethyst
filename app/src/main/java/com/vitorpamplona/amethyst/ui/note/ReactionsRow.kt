7be2d08db17777e9bd4c3c441b23feb534d98358
Vitor Pamplona <vitor@vitorpamplona.com>
2023-01-21 21:14:10 -0300

    View Count

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/ReactionsRow.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/ReactionsRow.kt
@@ -1,25 +1,40 @@
 package com.vitorpamplona.amethyst.ui.note
 
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.BarChart
+import androidx.compose.material.icons.outlined.Visibility
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.imageLoader
+import coil.request.CachePolicy
+import coil.request.ImageRequest
 import com.vitorpamplona.amethyst.R
 import com.vitorpamplona.amethyst.model.Account
 import com.vitorpamplona.amethyst.model.Note
@@ -37,7 +52,9 @@ fun ReactionsRow(note: Note, account: Account, boost: (Note) -> Unit, reactTo: (
     NewPostView({ wantsToReplyTo = null }, wantsToReplyTo, account)
 
   Row(
-    modifier = Modifier.padding(top = 8.dp).fillMaxWidth(),
+    modifier = Modifier
+      .padding(top = 8.dp)
+      .fillMaxWidth(),
     verticalAlignment = Alignment.CenterVertically,
     horizontalArrangement = Arrangement.SpaceBetween
   ) {
@@ -116,25 +133,35 @@ fun ReactionsRow(note: Note, account: Account, boost: (Note) -> Unit, reactTo: (
       modifier = Modifier.weight(1f)
     )
 
-    /*
-    Row(
-      modifier = Modifier.padding(start = 40.dp),
-      verticalAlignment = Alignment.CenterVertically
+
+    IconButton(
+      modifier = Modifier.then(Modifier.size(24.dp)),
+      onClick = { }
     ) {
-      IconButton(
-        modifier = Modifier.then(Modifier.size(24.dp)),
-        onClick = { }
-      ) {
-        Icon(
-          painter = painterResource(R.drawable.ic_share),
-          null,
-          modifier = Modifier.size(16.dp),
-          tint = grayTint
-        )
-      }
+      Icon(
+        imageVector = Icons.Outlined.BarChart,
+        null,
+        modifier = Modifier.size(19.dp),
+        tint = grayTint
+      )
     }
 
-     */
+    Row(modifier = Modifier.weight(1f)) {
+      AsyncImage(
+        model = ImageRequest.Builder(LocalContext.current)
+          .data("https://counter.amethyst.social/${note.idHex}.svg?label=+&color=00000000")
+          .crossfade(true)
+          .diskCachePolicy(CachePolicy.DISABLED)
+          .memoryCachePolicy(CachePolicy.ENABLED)
+          .build(),
+        contentDescription = "View count",
+        modifier = Modifier.height(24.dp),
+        colorFilter = ColorFilter.tint(grayTint)
+      )
+    }
+
+
+
   }
 }
 
