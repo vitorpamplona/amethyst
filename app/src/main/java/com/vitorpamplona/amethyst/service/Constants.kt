4e74ca6ed0e67c8e5f021f742902b03d7bb224f0
Vitor Pamplona <vitor@vitorpamplona.com>
2023-01-26 22:14:12 -0300

    Removing BRB due to the crashes.

--- app/src/main/java/com/vitorpamplona/amethyst/service/Constants.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/Constants.kt
@@ -6,7 +6,7 @@ object Constants {
     val defaultRelays = arrayOf(
       Relay("wss://nostr.bitcoiner.social", read = true, write = true),
       Relay("wss://relay.nostr.bg", read = true, write = true),
-      Relay("wss://brb.io", read = true, write = true),
+      //Relay("wss://brb.io", read = true, write = true),
       Relay("wss://relay.snort.social", read = true, write = true),
       Relay("wss://nostr.rocks", read = true, write = true),
       Relay("wss://relay.damus.io", read = true, write = true),
