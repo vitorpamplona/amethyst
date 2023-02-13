e5ee367761aed52e77d45c6ff609cdcced98ceab
Vitor Pamplona <vitor@vitorpamplona.com>
2023-02-12 21:48:07 -0500

    Doesn't start the Global to make sure home updates faster.

--- app/src/main/java/com/vitorpamplona/amethyst/ServiceManager.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ServiceManager.kt
@@ -45,7 +45,7 @@ object ServiceManager {
       NostrUserProfileFollowersDataSource.account = myAccount
 
       NostrAccountDataSource.start()
-      NostrGlobalDataSource.start()
+      //NostrGlobalDataSource.start()
       NostrHomeDataSource.start()
       NostrNotificationDataSource.start()
       NostrSingleEventDataSource.start()
