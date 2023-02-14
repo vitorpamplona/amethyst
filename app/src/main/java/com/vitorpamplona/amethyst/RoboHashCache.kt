5e5ea32be1738866f102e970427268f4b33de51c
Vitor Pamplona <vitor@vitorpamplona.com>
2023-02-14 14:25:01 -0500

    Better fixed avatar handling.

--- app/src/main/java/com/vitorpamplona/amethyst/RoboHashCache.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/RoboHashCache.kt
@@ -3,6 +3,8 @@ package com.vitorpamplona.amethyst
 import android.content.Context
 import android.graphics.Bitmap
 import android.util.LruCache
+import androidx.compose.ui.graphics.ImageBitmap
+import androidx.compose.ui.graphics.asImageBitmap
 import java.util.UUID
 import name.neuhalfen.projects.android.robohash.RoboHash
 
@@ -10,14 +12,18 @@ object RoboHashCache {
 
   lateinit var robots: RoboHash
 
+  lateinit var defaultAvatar: ImageBitmap
+
   @Synchronized
-  fun get(context: Context, hash: String): Bitmap {
+  fun get(context: Context, hash: String): ImageBitmap {
     if (!this::robots.isInitialized) {
       robots = RoboHash(context)
-      robots.useCache(LruCache(100));
+      //robots.useCache(LruCache(100));
+
+      defaultAvatar = robots.imageForHandle(robots.calculateHandleFromUUID(UUID.nameUUIDFromBytes("aaaa".toByteArray()))).asImageBitmap()
     }
 
-    return robots.imageForHandle(robots.calculateHandleFromUUID(UUID.nameUUIDFromBytes("aaaa".toByteArray())))
+    return defaultAvatar
   }
 
 }
