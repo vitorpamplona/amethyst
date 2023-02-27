bb3d8e0041ef4d5247c8f9de3a63e66927663f7c
Vitor Pamplona <vitor@vitorpamplona.com>
2023-02-27 17:14:37 -0500

    Filtering Notifications to the events that cite the user directly.

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/NotificationFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/NotificationFeedFilter.kt
@@ -5,18 +5,64 @@ import com.vitorpamplona.amethyst.model.Note
 import com.vitorpamplona.amethyst.service.model.ChannelCreateEvent
 import com.vitorpamplona.amethyst.service.model.ChannelMetadataEvent
 import com.vitorpamplona.amethyst.service.model.LnZapRequestEvent
+import com.vitorpamplona.amethyst.service.model.ReactionEvent
+import com.vitorpamplona.amethyst.service.model.RepostEvent
+import nostr.postr.events.TextNoteEvent
 
 object NotificationFeedFilter: FeedFilter<Note>() {
   lateinit var account: Account
 
   override fun feed(): List<Note> {
     return account.userProfile().taggedPosts
-      .filter { it.author == null || (!account.isHidden(it.author!!) && it.author != account.userProfile()) }
+      .filter {
+           it.author == null
+        || (!account.isHidden(it.author!!) && it.author != account.userProfile())
+      }
       .filter {
            it.event !is ChannelCreateEvent
         && it.event !is ChannelMetadataEvent
         && it.event !is LnZapRequestEvent
       }
+      .filter {
+        it.event !is TextNoteEvent
+        ||
+           (
+             it.event is TextNoteEvent
+             &&
+             (
+               it.replyTo?.lastOrNull()?.author == account.userProfile()
+                 ||
+               account.userProfile() in it.directlyCiteUsers()
+             )
+           )
+      }
+      .filter {
+        it.event !is ReactionEvent
+          ||
+          (
+            it.event is ReactionEvent
+              &&
+              (
+                it.replyTo?.lastOrNull()?.author == account.userProfile()
+                  ||
+                  account.userProfile() in it.directlyCiteUsers()
+                )
+            )
+      }
+      .filter {
+        it.event !is RepostEvent
+          ||
+          (
+            it.event is RepostEvent
+              &&
+              (
+                it.replyTo?.lastOrNull()?.author == account.userProfile()
+                  ||
+                  account.userProfile() in it.directlyCiteUsers()
+                )
+            )
+      }
+
       .sortedBy { it.event?.createdAt }
       .reversed()
   }
