b35a59372c4838ffbd7eb8184d3aa4df41839402
Chemaclass <chemaclass@outlook.es>
2023-03-03 23:23:42 +0100

    Prepare move unit tests for uri_to_route behaviour

--- app/src/test/java/com/vitorpamplona/amethyst/service/Nip19Test.kt
+++ app/src/test/java/com/vitorpamplona/amethyst/service/Nip19Test.kt
@@ -47,10 +47,62 @@ class Nip19Test {
 
   @Test()
   fun uri_to_route_npub() {
-    val actual = nip19.uriToRoute("nostr:npub1hv7k2s755n697sptva8vkh9jz40lzfzklnwj6ekewfmxp5crwdjs27007y")
+    val actual =
+      nip19.uriToRoute("nostr:npub1hv7k2s755n697sptva8vkh9jz40lzfzklnwj6ekewfmxp5crwdjs27007y")
 
     Assert.assertEquals(Nip19.Type.USER, actual?.type)
-    Assert.assertEquals("bb3d6543d4a4f45f402b674ecb5cb2155ff12456fcdd2d66d9727660d3037365", actual?.hex)
+    Assert.assertEquals(
+      "bb3d6543d4a4f45f402b674ecb5cb2155ff12456fcdd2d66d9727660d3037365",
+      actual?.hex
+    )
+  }
+
+  @Test()
+  fun uri_to_route_note() {
+    val actual =
+      nip19.uriToRoute("nostr:note1stqea6wmwezg9x6yyr6qkukw95ewtdukyaztycws65l8wppjmtpscawevv")
+
+    Assert.assertEquals(Nip19.Type.NOTE, actual?.type)
+    Assert.assertEquals(
+      "82c19ee9db7644829b4420f40b72ce2d32e5b7962744b261d0d53e770432dac3",
+      actual?.hex
+    )
+  }
+
+  @Ignore("Not implemented yet")
+  @Test()
+  fun uri_to_route_nprofile() {
+    val actual = nip19.uriToRoute("nostr:nprofile")
+
+    Assert.assertEquals(Nip19.Type.USER, actual?.type)
+    Assert.assertEquals("*", actual?.hex)
+  }
+
+  @Ignore("Not implemented yet")
+  @Test()
+  fun uri_to_route_nevent() {
+    val actual = nip19.uriToRoute("nostr:nevent")
+
+    Assert.assertEquals(Nip19.Type.USER, actual?.type)
+    Assert.assertEquals("*", actual?.hex)
+  }
+
+  @Ignore("Not implemented yet")
+  @Test()
+  fun uri_to_route_nrelay() {
+    val actual = nip19.uriToRoute("nostr:nrelay")
+
+    Assert.assertEquals(Nip19.Type.RELAY, actual?.type)
+    Assert.assertEquals("*", actual?.hex)
+  }
+
+  @Ignore("Not implemented yet")
+  @Test()
+  fun uri_to_route_naddr() {
+    val actual = nip19.uriToRoute("nostr:naddr")
+
+    Assert.assertEquals(Nip19.Type.ADDRESS, actual?.type)
+    Assert.assertEquals("*", actual?.hex)
   }
 
   private fun byteArrayOfInts(vararg ints: Int) = ByteArray(ints.size) { pos -> ints[pos].toByte() }
