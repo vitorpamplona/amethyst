12570d3e26e68246c7490a815883e8e2d2303272
Vitor Pamplona <vitor@vitorpamplona.com>
2023-03-05 13:02:26 -0500

    Fixing for https://github.com/vitorpamplona/amethyst/pull/201#pullrequestreview-1325255540

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/ChannelFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/ChannelFeedFilter.kt
@@ -17,9 +17,9 @@ object ChannelFeedFilter: FeedFilter<Note>() {
   // returns the last Note of each user.
   override fun feed(): List<Note> {
     return channel.notes
-      ?.values
-      ?.filter { account.isAcceptable(it) }
-      ?.sortedBy { it.createdAt() }
-      ?.reversed() ?: emptyList()
+      .values
+      .filter { account.isAcceptable(it) }
+      .sortedBy { it.createdAt() }
+      .reversed()
   }
 }
