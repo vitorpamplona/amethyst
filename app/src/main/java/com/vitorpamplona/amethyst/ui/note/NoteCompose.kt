6acc842d4f7284d08a2a5c5a5afb149e8e1268ef
Vitor Pamplona <vitor@vitorpamplona.com>
2023-03-08 17:18:25 -0500

    Readding LocalCacheImport

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/NoteCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/NoteCompose.kt
@@ -37,6 +37,7 @@ import com.google.accompanist.flowlayout.FlowRow
 import com.vitorpamplona.amethyst.NotificationCache
 import com.vitorpamplona.amethyst.R
 import com.vitorpamplona.amethyst.RoboHashCache
+import com.vitorpamplona.amethyst.model.LocalCache
 import com.vitorpamplona.amethyst.model.Note
 import com.vitorpamplona.amethyst.model.User
 import com.vitorpamplona.amethyst.service.model.BadgeAwardEvent
