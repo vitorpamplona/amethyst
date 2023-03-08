5055afd979eb685412cb8a3a363e005c4e49fdd5
Vitor Pamplona <vitor@vitorpamplona.com>
2023-03-08 13:14:06 -0500

    Fixes the build

--- app/src/test/java/com/vitorpamplona/amethyst/KeyParseTest.kt
+++ app/src/test/java/com/vitorpamplona/amethyst/KeyParseTest.kt
@@ -13,80 +13,80 @@ class KeyParseTest {
     @Test
     fun keyParseTestNote() {
         val result = parseDirtyWordForKey("note1z5e2m0smx6d7e2d0zaq8d3rnd7httm6j0uf8tf90yqqjrs842czshwtkmn")
-        assertEquals("note", result?.type)
-        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.keyHex)
+        assertEquals("note", result?.key?.type)
+        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.key?.hex)
         assertEquals("", result?.restOfWord)
     }
 
     @Test
     fun keyParseTestPub() {
         val result = parseDirtyWordForKey("npub1gcxzte5zlkncx26j68ez60fzkvtkm9e0vrwdcvsjakxf9mu9qewqlfnj5z")
-        assertEquals("npub", result?.type)
-        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.keyHex)
+        assertEquals("npub", result?.key?.type)
+        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.key?.hex)
         assertEquals("", result?.restOfWord)
     }
 
     @Test
     fun keyParseTestNoteWithExtraChars() {
         val result = parseDirtyWordForKey("note1z5e2m0smx6d7e2d0zaq8d3rnd7httm6j0uf8tf90yqqjrs842czshwtkmn,")
-        assertEquals("note", result?.type)
-        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.keyHex)
+        assertEquals("note", result?.key?.type)
+        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.key?.hex)
         assertEquals(",", result?.restOfWord)
     }
 
     @Test
     fun keyParseTestPubWithExtraChars() {
         val result = parseDirtyWordForKey("npub1gcxzte5zlkncx26j68ez60fzkvtkm9e0vrwdcvsjakxf9mu9qewqlfnj5z,")
-        assertEquals("npub", result?.type)
-        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.keyHex)
+        assertEquals("npub", result?.key?.type)
+        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.key?.hex)
         assertEquals(",", result?.restOfWord)
     }
 
     @Test
     fun keyParseTestNoteWithExtraCharsAndAt() {
         val result = parseDirtyWordForKey("@note1z5e2m0smx6d7e2d0zaq8d3rnd7httm6j0uf8tf90yqqjrs842czshwtkmn,")
-        assertEquals("note", result?.type)
-        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.keyHex)
+        assertEquals("note", result?.key?.type)
+        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.key?.hex)
         assertEquals(",", result?.restOfWord)
     }
 
     @Test
     fun keyParseTestPubWithExtraCharsAndAt() {
         val result = parseDirtyWordForKey("@npub1gcxzte5zlkncx26j68ez60fzkvtkm9e0vrwdcvsjakxf9mu9qewqlfnj5z,")
-        assertEquals("npub", result?.type)
-        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.keyHex)
+        assertEquals("npub", result?.key?.type)
+        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.key?.hex)
         assertEquals(",", result?.restOfWord)
     }
 
     @Test
     fun keyParseTestNoteWithExtraCharsAndNostrPrefix() {
         val result = parseDirtyWordForKey("nostr:note1z5e2m0smx6d7e2d0zaq8d3rnd7httm6j0uf8tf90yqqjrs842czshwtkmn,")
-        assertEquals("note", result?.type)
-        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.keyHex)
+        assertEquals("note", result?.key?.type)
+        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.key?.hex)
         assertEquals(",", result?.restOfWord)
     }
 
     @Test
     fun keyParseTestPubWithExtraCharsAndNostrPrefix() {
         val result = parseDirtyWordForKey("nostr:npub1gcxzte5zlkncx26j68ez60fzkvtkm9e0vrwdcvsjakxf9mu9qewqlfnj5z,")
-        assertEquals("npub", result?.type)
-        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.keyHex)
+        assertEquals("npub", result?.key?.type)
+        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.key?.hex)
         assertEquals(",", result?.restOfWord)
     }
 
     @Test
     fun keyParseTestUppercaseNoteWithExtraCharsAndNostrPrefix() {
         val result = parseDirtyWordForKey("Nostr:note1z5e2m0smx6d7e2d0zaq8d3rnd7httm6j0uf8tf90yqqjrs842czshwtkmn,")
-        assertEquals("note", result?.type)
-        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.keyHex)
+        assertEquals("note", result?.key?.type)
+        assertEquals("1532adbe1b369beca9af174076c4736faeb5ef527f1275a4af200121c0f55605", result?.key?.hex)
         assertEquals(",", result?.restOfWord)
     }
 
     @Test
     fun keyParseTestUppercasePubWithExtraCharsAndNostrPrefix() {
         val result = parseDirtyWordForKey("nOstr:npub1gcxzte5zlkncx26j68ez60fzkvtkm9e0vrwdcvsjakxf9mu9qewqlfnj5z,")
-        assertEquals("npub", result?.type)
-        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.keyHex)
+        assertEquals("npub", result?.key?.type)
+        assertEquals("460c25e682fda7832b52d1f22d3d22b3176d972f60dcdc3212ed8c92ef85065c", result?.key?.hex)
         assertEquals(",", result?.restOfWord)
     }
 }
