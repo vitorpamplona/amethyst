7c94af2fb26535be498e472d26178df3b4d8df9b
Chemaclass <chemaclass@outlook.es>
2023-03-09 20:56:56 +0000

    Use MasterKey.Builder instead of deprecated MasterKeys

--- app/src/main/java/com/vitorpamplona/amethyst/EncryptedStorage.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/EncryptedStorage.kt
@@ -2,18 +2,20 @@ package com.vitorpamplona.amethyst
 
 import android.content.Context
 import androidx.security.crypto.EncryptedSharedPreferences
-import androidx.security.crypto.MasterKeys
+import androidx.security.crypto.MasterKey
 
 class EncryptedStorage {
+    private val preferencesName = "secret_keeper"
 
     fun preferences(context: Context): EncryptedSharedPreferences {
-        val secretKey: String = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
-        val preferencesName = "secret_keeper"
+        val masterKey: MasterKey = MasterKey.Builder(context, MasterKey.DEFAULT_MASTER_KEY_ALIAS)
+            .setKeyScheme(MasterKey.KeyScheme.AES256_GCM)
+            .build()
 
         return EncryptedSharedPreferences.create(
-            preferencesName,
-            secretKey,
             context,
+            preferencesName,
+            masterKey,
             EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
             EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         ) as EncryptedSharedPreferences
