b1cb7d599fd5b92adc0bd81f74f67fefb13dd029
maxmoney21m <maxmoney@nostrplebs.com>
2023-03-12 20:34:50 +0800

    Make account switcher scrollable

--- app/src/main/java/com/vitorpamplona/amethyst/ui/navigation/AccountSwitchBottomSheet.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/navigation/AccountSwitchBottomSheet.kt
@@ -11,7 +11,9 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
@@ -64,8 +66,9 @@ fun AccountSwitchBottomSheet(
     val accountUser = accountUserState?.user ?: return
 
     var popupExpanded by remember { mutableStateOf(false) }
+    val scrollState = rememberScrollState()
 
-    Column {
+    Column(modifier = Modifier.verticalScroll(scrollState)) {
         Row(
             modifier = Modifier
                 .fillMaxWidth()
