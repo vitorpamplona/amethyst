83a8558f23ff59ed5172900095c7a134b8c98fa2
Vitor Pamplona <vitor@vitorpamplona.com>
2023-03-15 13:39:47 -0400

    Caching followingChannels and followingUsers to speed Global filter up

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/GlobalFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/GlobalFeedFilter.kt
@@ -10,20 +10,25 @@ import com.vitorpamplona.amethyst.service.model.TextNoteEvent
 object GlobalFeedFilter : FeedFilter<Note>() {
     lateinit var account: Account
 
-    override fun feed() = LocalCache.notes.values
-        .asSequence()
-        .filter {
-            (it.event is TextNoteEvent || it.event is LongTextNoteEvent || it.event is ChannelMessageEvent) &&
-                it.replyTo.isNullOrEmpty()
-        }
-        .filter {
-            // does not show events already in the public chat list
-            (it.channel() == null || it.channel() !in account.followingChannels()) &&
-                // does not show people the user already follows
-                (it.author?.pubkeyHex !in account.followingKeySet())
-        }
-        .filter { account.isAcceptable(it) }
-        .sortedBy { it.createdAt() }
-        .toList()
-        .reversed()
+    override fun feed(): List<Note> {
+        val followChannels = account.followingChannels()
+        val followUsers = account.followingKeySet()
+
+        return LocalCache.notes.values
+            .asSequence()
+            .filter {
+                (it.event is TextNoteEvent || it.event is LongTextNoteEvent || it.event is ChannelMessageEvent) &&
+                    it.replyTo.isNullOrEmpty()
+            }
+            .filter {
+                // does not show events already in the public chat list
+                (it.channel() == null || it.channel() !in followChannels) &&
+                    // does not show people the user already follows
+                    (it.author?.pubkeyHex !in followUsers)
+            }
+            .filter { account.isAcceptable(it) }
+            .sortedBy { it.createdAt() }
+            .toList()
+            .reversed()
+    }
 }
