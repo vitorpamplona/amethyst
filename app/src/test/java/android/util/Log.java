c6906c42fdc4f17979a118861f3fc3d609528bc5
Vitor Pamplona <vitor@vitorpamplona.com>
2023-03-17 09:47:47 -0400

    Mocking Android.Log for test classes

---
+++ app/src/test/java/android/util/Log.java
@@ -0,0 +1,35 @@
+package android.util;
+
+public class Log {
+    public static int d(String tag, String msg) {
+        System.out.println("DEBUG: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int i(String tag, String msg) {
+        System.out.println("INFO: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int w(String tag, String msg) {
+        System.out.println("WARN: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int w(String tag, String msg, Throwable e) {
+        System.out.println("WARN: " + tag + ": " + msg);
+        e.printStackTrace();
+        return 0;
+    }
+
+    public static int e(String tag, String msg) {
+        System.out.println("ERROR: " + tag + ": " + msg);
+        return 0;
+    }
+
+    public static int e(String tag, String msg, Throwable e) {
+        System.out.println("ERROR: " + tag + ": " + msg);
+        e.printStackTrace();
+        return 0;
+    }
+}
