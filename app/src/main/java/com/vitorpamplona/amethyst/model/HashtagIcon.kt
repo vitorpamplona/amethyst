f266a9e964218e11010e5430695be2d22f098509
Believethehype <tobias_baur@yahoo.com>
2023-03-17 12:38:47 +0100

    Special Hashtag for #Nostr

--- app/src/main/java/com/vitorpamplona/amethyst/model/HashtagIcon.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/model/HashtagIcon.kt
@@ -4,7 +4,9 @@ import androidx.compose.ui.graphics.Color
 import com.vitorpamplona.amethyst.R
 fun checkForHashtagWithIcon(tag: String): HashtagIcon? {
     if (tag.lowercase() == "bitcoin" || tag.lowercase() == "btc") {
-        return HashtagIcon(R.drawable.ht_btc, "Bitcoin", Color(0xFFf2A900))
+        return HashtagIcon(R.drawable.ht_btc, "Bitcoin", Color(0xFFF2A900))
+    } else if (tag.lowercase() == "nostr") {
+        return HashtagIcon(R.drawable.ht_nostr, "Nostr", Color(0xFF9C59FF))
     }
     return null
 }
