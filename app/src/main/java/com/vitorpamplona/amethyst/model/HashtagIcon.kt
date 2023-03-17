88292aa179f431a298b53466a194294fe1652fc4
Believethehype <tobias_baur@yahoo.com>
2023-03-17 23:34:59 +0100

    updated icons

--- app/src/main/java/com/vitorpamplona/amethyst/model/HashtagIcon.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/model/HashtagIcon.kt
@@ -4,13 +4,13 @@ import androidx.compose.ui.graphics.Color
 import com.vitorpamplona.amethyst.R
 fun checkForHashtagWithIcon(tag: String): HashtagIcon? {
     if (tag.lowercase() == "bitcoin" || tag.lowercase() == "btc") {
-        return HashtagIcon(R.drawable.ht_btc, "Bitcoin", Color(0xFFF2A900))
+        return HashtagIcon(R.drawable.ht_btc, "Bitcoin", Color.Unspecified)
     } else if (tag.lowercase() == "nostr") {
-        return HashtagIcon(R.drawable.ht_nostr, "Nostr", Color(0xFF9C59FF))
-    } else if (tag.lowercase() == "zap" || tag.lowercase() == "zapathon" || tag.lowercase() == "zapraiser" || tag.lowercase() == "zaplife" || tag.lowercase() == "lightning") {
-        return HashtagIcon(R.drawable.ht_zap, "Zap", Color(0xFFFFEC01))
+        return HashtagIcon(R.drawable.ht_nostr, "Nostr", Color.Unspecified)
+    } else if (tag.lowercase() == "zap" || tag.lowercase() == "zap" || tag.lowercase() == "zapathon" || tag.lowercase() == "zapraiser" || tag.lowercase() == "zaplife" || tag.lowercase() == "lightning") {
+        return HashtagIcon(R.drawable.lightning, "Zap", Color.Unspecified)
     } else if (tag.lowercase() == "amethyst") {
-        return HashtagIcon(R.drawable.ht_amethyst, "Amethyst", Color(0xFFb793e6))
+        return HashtagIcon(R.drawable.amethyst, "Amethyst", Color.Unspecified)
     }
     return null
 }
