3ba979e513cb8fb93ea4efe556f2dd39afa0e94b
Believethehype <tobias_baur@yahoo.com>
2023-03-21 00:55:27 +0100

    Update HashtagIcon.kt

--- app/src/main/java/com/vitorpamplona/amethyst/model/HashtagIcon.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/model/HashtagIcon.kt
@@ -9,7 +9,7 @@ fun checkForHashtagWithIcon(tag: String): HashtagIcon? {
         "bitcoin", "btc" -> HashtagIcon(R.drawable.ht_btc, "Bitcoin", Color.Unspecified, Modifier.padding(2.dp, 2.dp, 0.dp, 0.dp))
         "nostr" -> HashtagIcon(R.drawable.ht_nostr, "Nostr", Color.Unspecified, Modifier.padding(1.dp, 2.dp, 0.dp, 0.dp))
         "lightning", "lightningnetwork" -> HashtagIcon(R.drawable.ht_lightning, "Lightning", Color.Unspecified, Modifier.padding(1.dp, 2.dp, 0.dp, 0.dp))
-        "zap", "zaps", "zapathon", "zapraiser", "zaplife" -> HashtagIcon(R.drawable.zap, "Zap", Color.Unspecified, Modifier.padding(1.dp, 2.dp, 0.dp, 0.dp))
+        "zap", "zaps", "zapathon", "zapraiser", "zaplife", "zapper", "zappers" -> HashtagIcon(R.drawable.zap, "Zap", Color.Unspecified, Modifier.padding(1.dp, 2.dp, 0.dp, 0.dp))
         "amethyst" -> HashtagIcon(R.drawable.amethyst, "Amethyst", Color.Unspecified, Modifier.padding(4.dp, 2.dp, 0.dp, 0.dp))
         else -> null
     }
