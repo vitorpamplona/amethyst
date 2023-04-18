e4fc00669e26e824ec0f2b47128dd84eed772716
Vitor Pamplona <vitor@vitorpamplona.com>
2023-04-18 08:44:32 -0400

    Compares users by reference.

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/MultiSetCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/MultiSetCompose.kt
@@ -244,7 +244,7 @@ fun FastNoteAuthorPicture(
     val userState by author.live().metadata.observeAsState()
     val user = userState?.user ?: return
 
-    val showFollowingMark = userAccount.isFollowingCached(user) || user == userAccount
+    val showFollowingMark = userAccount.isFollowingCached(user) || user === userAccount
 
     UserPicture(
         userHex = user.pubkeyHex,
