a013f1f1cc71c0d6dc567705c33cbec2e984df44
Believethehype <tobias_baur@yahoo.com>
2023-04-22 20:11:05 +0200

    added new hashtag icons

--- app/src/main/java/com/vitorpamplona/amethyst/model/HashtagIcon.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/model/HashtagIcon.kt
@@ -14,14 +14,8 @@ fun checkForHashtagWithIcon(tag: String): HashtagIcon? {
         "plebs", "pleb", "plebchain" -> HashtagIcon(R.drawable.plebs, "Pleb", Color.Unspecified, Modifier.padding(2.dp, 2.dp, 0.dp, 1.dp))
         "coffee", "coffeechain", "cafe" -> HashtagIcon(R.drawable.coffee, "Coffee", Color.Unspecified, Modifier.padding(2.dp, 2.dp, 0.dp, 0.dp))
         "skullofsatoshi" -> HashtagIcon(R.drawable.skull, "SkullofSatoshi", Color.Unspecified, Modifier.padding(2.dp, 1.dp, 0.dp, 0.dp))
-        "foodstr", "pizza" -> HashtagIcon(R.drawable.foodstr, "Foodstr", Color.Unspecified, Modifier.padding(2.dp, 1.dp, 0.dp, 0.dp))
-        "dogstr", "npups" -> HashtagIcon(R.drawable.dogstr, "Dogstr", Color.Unspecified, Modifier.padding(0.dp, 1.dp, 0.dp, 0.dp))
-        "catstr", "catestr" -> HashtagIcon(R.drawable.catstr, "Catstr", Color.Unspecified, Modifier.padding(1.dp, 4.dp, 0.dp, 1.dp))
-        "footstr", "pawstr" -> HashtagIcon(R.drawable.footstr, "Footstr", Color.Unspecified, Modifier.padding(1.dp, 1.dp, 0.dp, 0.dp))
-        "bookstr" -> HashtagIcon(R.drawable.bookstr, "Bookstr", Color.Unspecified, Modifier.padding(1.dp, 0.dp, 0.dp, 0.dp))
-        "babechain", "kiss" -> HashtagIcon(R.drawable.kiss, "Babechain", Color.Unspecified, Modifier.padding(1.dp, 0.dp, 0.dp, 0.dp))
-        "grownostr", "#permies", "#perma", "#permaculture", "#gardening", "#garden" -> HashtagIcon(R.drawable.grownostr, "GrowNostr", Color.Unspecified, Modifier.padding(0.dp, 1.dp, 0.dp, 1.dp))
-
+        "grownostr", "#permaculture", "#gardening", "#garden" -> HashtagIcon(R.drawable.grownostr, "GrowNostr", Color.Unspecified, Modifier.padding(0.dp, 1.dp, 0.dp, 1.dp))
+        "footstr" -> HashtagIcon(R.drawable.footstr, "Footstr", Color.Unspecified, Modifier.padding(1.dp, 1.dp, 0.dp, 0.dp))
         else -> null
     }
 }
