e07b4d49ef46e21676db034df71483ff4561eff5
Vitor Pamplona <vitor@vitorpamplona.com>
2023-05-15 13:10:33 -0400

    Fixes NPE on opening the QuickAction on a "Post Not Found" event.

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/NoteQuickActionMenu.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/NoteQuickActionMenu.kt
@@ -128,7 +128,7 @@ fun NoteQuickActionMenu(note: Note, popupExpanded: Boolean, onDismiss: () -> Uni
 
     if (popupExpanded) {
         val isOwnNote = accountViewModel.isLoggedUser(note.author)
-        val isFollowingUser = !isOwnNote && accountViewModel.isFollowing(note.author!!)
+        val isFollowingUser = !isOwnNote && accountViewModel.isFollowing(note.author)
 
         Popup(onDismissRequest = onDismiss) {
             Card(
