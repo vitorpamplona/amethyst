a57ca2a79db66ded7be90240edb32fc492691d7a
Vitor Pamplona <vitor@vitorpamplona.com>
2023-05-31 16:33:23 -0400

    Using the correct immutable class

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/ThreadFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/ThreadFeedFilter.kt
@@ -1,6 +1,6 @@
 package com.vitorpamplona.amethyst.ui.dal
 
-import com.google.errorprone.annotations.Immutable
+import androidx.compose.runtime.Immutable
 import com.vitorpamplona.amethyst.model.Note
 import com.vitorpamplona.amethyst.model.ThreadAssembler
 
