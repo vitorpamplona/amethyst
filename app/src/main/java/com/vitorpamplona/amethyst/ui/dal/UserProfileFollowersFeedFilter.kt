839be06f625386fd5cbc5d7b13e4d2d070d3135c
Vitor Pamplona <vitor@vitorpamplona.com>
2023-06-02 21:59:17 -0400

    Removes unnecessary mapping

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/UserProfileFollowersFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/UserProfileFollowersFeedFilter.kt
@@ -7,8 +7,6 @@ import com.vitorpamplona.amethyst.model.User
 class UserProfileFollowersFeedFilter(val user: User, val account: Account) : FeedFilter<User>() {
 
     override fun feed(): List<User> {
-        return user.let { myUser ->
-            LocalCache.users.values.filter { it.isFollowing(myUser) && !account.isHidden(it) }
-        }
+        return LocalCache.users.values.filter { it.isFollowing(user) && !account.isHidden(it) }
     }
 }
