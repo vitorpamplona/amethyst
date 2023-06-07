4bd19c3e3da08cc99146b9f37cebc7a8152e06ed
Vitor Pamplona <vitor@vitorpamplona.com>
2023-06-07 12:07:03 -0400

    Deletes hiddenUsers method from account to avoid misuse since it needs a blocking load from cache.

--- app/src/main/java/com/vitorpamplona/amethyst/ui/dal/HiddenAccountsFeedFilter.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/dal/HiddenAccountsFeedFilter.kt
@@ -1,9 +1,13 @@
 package com.vitorpamplona.amethyst.ui.dal
 
 import com.vitorpamplona.amethyst.model.Account
+import com.vitorpamplona.amethyst.model.LocalCache
 import com.vitorpamplona.amethyst.model.User
 
 class HiddenAccountsFeedFilter(val account: Account) : FeedFilter<User>() {
 
-    override fun feed() = account.hiddenUsers()
+    override fun feed(): List<User> {
+        return (account.hiddenUsers + account.transientHiddenUsers)
+            .map { LocalCache.getOrCreateUser(it) }
+    }
 }
