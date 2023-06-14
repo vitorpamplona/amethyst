5616fa52d12ee3c1a23665ac4cd3cf8873f4100a
Vitor Pamplona <vitor@vitorpamplona.com>
2023-06-14 13:48:35 -0400

    Simplifies URL Preview call

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/UrlPreview.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/UrlPreview.kt
@@ -8,56 +8,25 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
-import androidx.compose.ui.platform.LocalContext
-import com.vitorpamplona.amethyst.R
 import com.vitorpamplona.amethyst.model.UrlCachedPreviewer
-import com.vitorpamplona.amethyst.service.previews.IUrlPreviewCallback
-import com.vitorpamplona.amethyst.service.previews.UrlInfoItem
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 @Composable
 fun UrlPreview(url: String, urlText: String) {
-    val context = LocalContext.current
-
     var urlPreviewState by remember(url) {
-        val default = UrlCachedPreviewer.cache[url]?.let {
-            if (it.allFetchComplete() && it.url == url) {
-                UrlPreviewState.Loaded(it)
-            } else {
-                UrlPreviewState.Empty
-            }
-        } ?: UrlPreviewState.Loading
-
-        mutableStateOf(default)
+        mutableStateOf(
+            UrlCachedPreviewer.cache.get(url)?.let { it } ?: UrlPreviewState.Loading
+        )
     }
 
     // Doesn't use a viewModel because of viewModel reusing issues (too many UrlPreview are created).
     LaunchedEffect(url) {
         if (urlPreviewState == UrlPreviewState.Loading) {
             launch(Dispatchers.IO) {
-                UrlCachedPreviewer.previewInfo(
-                    url,
-                    object : IUrlPreviewCallback {
-                        override fun onComplete(urlInfo: UrlInfoItem) {
-                            if (urlInfo.allFetchComplete() && urlInfo.url == url) {
-                                urlPreviewState = UrlPreviewState.Loaded(urlInfo)
-                            } else {
-                                urlPreviewState = UrlPreviewState.Empty
-                            }
-                        }
-
-                        override fun onFailed(throwable: Throwable) {
-                            urlPreviewState = UrlPreviewState.Error(
-                                context.getString(
-                                    R.string.error_parsing_preview_for,
-                                    url,
-                                    throwable.message
-                                )
-                            )
-                        }
-                    }
-                )
+                UrlCachedPreviewer.previewInfo(url) {
+                    urlPreviewState = it
+                }
             }
         }
     }
