a1930c232b19331eb186fabb7faf05396747a926
Vitor Pamplona <vitor@vitorpamplona.com>
2023-06-18 14:42:26 -0400

    Formatting

--- app/src/main/java/com/vitorpamplona/amethyst/ui/components/ZapRaiserRequest.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/components/ZapRaiserRequest.kt
@@ -13,8 +13,6 @@ import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
