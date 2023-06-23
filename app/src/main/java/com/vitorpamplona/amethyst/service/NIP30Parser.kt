e8f5ef3aa3429e31bb280de315a0ed01ed33c080
Vitor Pamplona <vitor@vitorpamplona.com>
2023-06-23 12:10:46 -0400

    Removes check in the main thread to avoid mocking Android's threading system

--- app/src/main/java/com/vitorpamplona/amethyst/service/NIP30Parser.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/NIP30Parser.kt
@@ -8,8 +8,6 @@ class NIP30Parser {
     val customEmojiPattern: Pattern = Pattern.compile("\\:([A-Za-z0-9_\\-]+)\\:", Pattern.CASE_INSENSITIVE)
 
     fun buildArray(input: String): List<String> {
-        checkNotInMainThread()
-
         val matcher = customEmojiPattern.matcher(input)
         val list = mutableListOf<String>()
         while (matcher.find()) {
