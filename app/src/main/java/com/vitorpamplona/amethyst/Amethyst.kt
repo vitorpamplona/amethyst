eb73da1993b3f7de44a35a24bff3a9216305319e
Vitor Pamplona <vitor@vitorpamplona.com>
2023-07-22 18:26:12 -0400

    Activates Strict mode in debug

--- app/src/main/java/com/vitorpamplona/amethyst/Amethyst.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/Amethyst.kt
@@ -1,11 +1,35 @@
 package com.vitorpamplona.amethyst
 
 import android.app.Application
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
+import android.os.StrictMode.VmPolicy
 
 class Amethyst : Application() {
     override fun onCreate() {
         super.onCreate()
         instance = this
+
+        if (BuildConfig.DEBUG) {
+            StrictMode.setThreadPolicy(
+                ThreadPolicy.Builder()
+                    .detectAll()
+                    .penaltyLog()
+                    .build()
+            )
+            StrictMode.setVmPolicy(
+                VmPolicy.Builder()
+                    .detectActivityLeaks()
+                    .detectFileUriExposure()
+                    .detectContentUriWithoutPermission()
+                    .detectLeakedClosableObjects()
+                    .detectLeakedRegistrationObjects()
+                    .detectLeakedSqlLiteObjects()
+                    .penaltyLog()
+                    // .penaltyDeath()
+                    .build()
+            )
+        }
     }
 
     companion object {
