b84f6887b6d42c155ed737d358d10a8767005207
KotlinGeekDev <kotlingeek@protonmail.com>
2023-07-31 19:35:13 +0100

    Fixes group notifications. Applies messaging style to individual DM notifications as well.

--- app/src/main/java/com/vitorpamplona/amethyst/service/notifications/NotificationUtils.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/notifications/NotificationUtils.kt
@@ -5,10 +5,14 @@ import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
+import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
 import android.service.notification.StatusBarNotification
 import androidx.core.app.NotificationCompat
+import androidx.core.app.Person
+import androidx.core.graphics.drawable.IconCompat
+import androidx.core.graphics.drawable.toBitmap
 import coil.ImageLoader
 import coil.executeBlocking
 import coil.request.ImageRequest
@@ -18,6 +22,7 @@ import com.vitorpamplona.amethyst.ui.MainActivity
 object NotificationUtils {
     private var dmChannel: NotificationChannel? = null
     private var zapChannel: NotificationChannel? = null
+    private const val DMS_SUMMARY_ID = 1725900883 // This is the absolute value hashcode of "Amethyst", i.e "Amethyst".hashCode().absoluteValue
     private const val DM_GROUP_KEY = "com.vitorpamplona.amethyst.DM_NOTIFICATION"
     private const val ZAP_GROUP_KEY = "com.vitorpamplona.amethyst.ZAP_NOTIFICATION"
 
@@ -168,9 +173,16 @@ object NotificationUtils {
         )
             .setSmallIcon(R.drawable.amethyst)
             .setContentTitle(messageTitle)
+            .useMessagingStyleIf(
+                notificationGroupKey == DM_GROUP_KEY,
+                picture?.toBitmap(),
+                messageTitle,
+                messageBody
+            )
             .setContentText(applicationContext.getString(R.string.app_notification_private_message))
             .setLargeIcon(picture?.bitmap)
             .setGroup(notificationGroupKey)
+            .setGroupSummaryBehaviorIf(notificationGroupKey == DM_GROUP_KEY)
             .setContentIntent(contentPendingIntent)
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
@@ -182,15 +194,43 @@ object NotificationUtils {
         )
             .setSmallIcon(R.drawable.amethyst)
             .setContentTitle(messageTitle)
+            .useMessagingStyleIf(
+                notificationGroupKey == DM_GROUP_KEY,
+                picture?.toBitmap(),
+                messageTitle,
+                messageBody
+            )
             .setContentText(messageBody)
             .setLargeIcon(picture?.bitmap)
             .setGroup(notificationGroupKey)
+            .setGroupSummaryBehaviorIf(notificationGroupKey == DM_GROUP_KEY)
             .setContentIntent(contentPendingIntent)
             .setPublicVersion(builderPublic.build())
             .setPriority(NotificationCompat.PRIORITY_HIGH)
             .setAutoCancel(true)
 
+        // For getting the notifications grouped by user.
+        val userNotifications = notifications.groupBy { it.notification.extras.getString("android.title") }
+        val numberOfUsers = userNotifications.keys.filterNotNull().size
+        val numberOfDMs = userNotifications.values.sumOf { it.size }
+
+        // Builds the group(summary) notification
+        val summaryNotification = NotificationCompat.Builder(applicationContext, channelId)
+            .setSmallIcon(R.drawable.amethyst)
+            .setContentText("$numberOfDMs messages from $numberOfUsers users")
+            .setAutoCancel(true)
+            .setStyle(
+                NotificationCompat.InboxStyle()
+                    .setSummaryText("$numberOfDMs messages from $numberOfUsers users")
+            )
+            .setGroup(DM_GROUP_KEY)
+            .setGroupSummary(true)
+            .build()
+
         notify(notId, builder.build())
+        if (notificationGroupKey == DM_GROUP_KEY) {
+            notify(DMS_SUMMARY_ID, summaryNotification)
+        }
     }
 
     /**
@@ -198,5 +238,47 @@ object NotificationUtils {
      */
     fun NotificationManager.cancelNotifications() {
         cancelAll()
+    }
+
+    /**
+     * Applies the GROUP_ALERT_SUMMARY behaviour if condition is met. In this context,
+     * it is very useful for DM notifications, to let only the summary notification show, with
+     * the updated info from the individual DM notifications.
+     */
+    private fun NotificationCompat.Builder.setGroupSummaryBehaviorIf(condition: Boolean): NotificationCompat.Builder {
+        if (condition) {
+            return setGroupAlertBehavior(NotificationCompat.GROUP_ALERT_SUMMARY)
+        }
+        return this
+    }
+
+    /**
+     * Applies this style to really fit in with the messaging notification aesthetic,
+     * when the given condition is met.
+     * This applies particularly to DM notifications.
+     */
+    private fun NotificationCompat.Builder.useMessagingStyleIf(
+        condition: Boolean,
+        userPicture: Bitmap?,
+        messageTitle: String,
+        messageBody: String
+    ): NotificationCompat.Builder {
+        if (condition) {
+            val senderBuilder = Person.Builder()
+            senderBuilder.setName(messageTitle)
+            if (userPicture != null) {
+                senderBuilder.setIcon(IconCompat.createWithBitmap(userPicture))
+            }
+            val sender = senderBuilder.build()
+            return setStyle(
+                NotificationCompat.MessagingStyle(Person.Builder().setName("Me").build())
+                    .addMessage(
+                        messageBody,
+                        System.currentTimeMillis(),
+                        sender
+                    )
+            )
+        }
+        return this
     }
 }
