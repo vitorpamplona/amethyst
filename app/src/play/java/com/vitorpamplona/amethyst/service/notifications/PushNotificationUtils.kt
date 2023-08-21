453e63025d89c455f656760c8514a3a518b09260
greenart7c3 <greenart7c3@proton.me>
2023-08-21 15:58:01 -0300

    fix crash in PushNotificationUtils when theres no google play services

--- app/src/play/java/com/vitorpamplona/amethyst/service/notifications/PushNotificationUtils.kt
+++ app/src/play/java/com/vitorpamplona/amethyst/service/notifications/PushNotificationUtils.kt
@@ -1,5 +1,6 @@
 package com.vitorpamplona.amethyst.service.notifications
 
+import android.util.Log
 import com.google.firebase.messaging.FirebaseMessaging
 import com.vitorpamplona.amethyst.AccountInfo
 import kotlinx.coroutines.Dispatchers
@@ -12,6 +13,10 @@ object PushNotificationUtils {
             return@with
         }
         // get user notification token provided by firebase
-        RegisterAccounts(accounts).go(FirebaseMessaging.getInstance().token.await())
+        try {
+            RegisterAccounts(accounts).go(FirebaseMessaging.getInstance().token.await())
+        } catch (e: Exception) {
+            Log.e("Firebase token", "failed to get firebase token", e)
+        }
     }
 }
