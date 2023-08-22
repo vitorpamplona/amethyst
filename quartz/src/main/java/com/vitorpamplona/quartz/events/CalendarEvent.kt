af143f2ea1ca8b30fe747aae8b393881608022a4
Vitor Pamplona <vitor@vitorpamplona.com>
2023-08-22 16:19:59 -0400

    Base support for Calendar events

---
+++ quartz/src/main/java/com/vitorpamplona/quartz/events/CalendarEvent.kt
@@ -0,0 +1,36 @@
+package com.vitorpamplona.quartz.events
+
+import androidx.compose.runtime.Immutable
+import com.vitorpamplona.quartz.utils.TimeUtils
+import com.vitorpamplona.quartz.encoders.toHexKey
+import com.vitorpamplona.quartz.crypto.CryptoUtils
+import com.vitorpamplona.quartz.encoders.ATag
+import com.vitorpamplona.quartz.encoders.HexKey
+
+@Immutable
+class CalendarEvent(
+    id: HexKey,
+    pubKey: HexKey,
+    createdAt: Long,
+    tags: List<List<String>>,
+    content: String,
+    sig: HexKey
+) : Event(id, pubKey, createdAt, kind, tags, content, sig), AddressableEvent {
+    override fun dTag() = tags.firstOrNull { it.size > 1 && it[0] == "d" }?.get(1) ?: ""
+    override fun address() = ATag(kind, pubKey, dTag(), null)
+
+    companion object {
+        const val kind = 31924
+
+        fun create(
+            privateKey: ByteArray,
+            createdAt: Long = TimeUtils.now()
+        ): CalendarEvent {
+            val tags = mutableListOf<List<String>>()
+            val pubKey = CryptoUtils.pubkeyCreate(privateKey).toHexKey()
+            val id = generateId(pubKey, createdAt, kind, tags, "")
+            val sig = CryptoUtils.sign(id, privateKey)
+            return CalendarEvent(id.toHexKey(), pubKey, createdAt, tags, "", sig.toHexKey())
+        }
+    }
+}
