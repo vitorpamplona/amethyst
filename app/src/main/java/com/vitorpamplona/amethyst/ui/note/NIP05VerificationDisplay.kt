0b41110938e6f1012c699a72c8b17d3404cae8cc
Vitor Pamplona <vitor@vitorpamplona.com>
2023-08-27 15:53:41 -0400

    Fixes formatting

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/NIP05VerificationDisplay.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/NIP05VerificationDisplay.kt
@@ -161,7 +161,7 @@ fun RotateStatuses(
         LaunchedEffect(Unit) {
             while (true) {
                 delay(10.seconds)
-                indexToDisplay = ((indexToDisplay + 1) % (statuses.size))
+                indexToDisplay = (indexToDisplay + 1) % statuses.size
             }
         }
     }
