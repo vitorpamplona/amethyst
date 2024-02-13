c9d59fd5ac87aa02b68a6c761df34f364d635de2
Vitor Pamplona <vitor@vitorpamplona.com>
2024-02-13 11:38:28 -0500

    Adds a check for the main thread when pulling opengraph tags.

--- app/src/main/java/com/vitorpamplona/amethyst/service/previews/UrlPreviewUtils.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/service/previews/UrlPreviewUtils.kt
@@ -21,6 +21,7 @@
 package com.vitorpamplona.amethyst.service.previews
 
 import com.vitorpamplona.amethyst.service.HttpClientManager
+import com.vitorpamplona.amethyst.service.checkNotInMainThread
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import okhttp3.MediaType
@@ -91,6 +92,7 @@ suspend fun getDocument(
     withContext(Dispatchers.IO) {
         val request: Request = Request.Builder().url(url).get().build()
         HttpClientManager.getHttpClient().newCall(request).execute().use {
+            checkNotInMainThread()
             if (it.isSuccessful) {
                 val mimeType =
                     it.headers.get("Content-Type")?.toMediaType()
