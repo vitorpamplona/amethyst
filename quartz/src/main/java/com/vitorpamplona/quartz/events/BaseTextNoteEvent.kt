8f5820f46dd6b2015283aeb8fb02567fdc153f77
Vitor Pamplona <vitor@vitorpamplona.com>
2024-03-21 16:54:50 -0400

    Fixes the reply finder for the reply row on compose

--- quartz/src/main/java/com/vitorpamplona/quartz/events/BaseTextNoteEvent.kt
+++ quartz/src/main/java/com/vitorpamplona/quartz/events/BaseTextNoteEvent.kt
@@ -72,7 +72,7 @@ open class BaseTextNoteEvent(
     }
 
     fun replyingTo(): HexKey? {
-        val oldStylePositional = tags.lastOrNull { it.size > 1 && it[0] == "e" }?.get(1)
+        val oldStylePositional = tags.lastOrNull { it.size > 1 && it.size <= 3 && it[0] == "e" }?.get(1)
         val newStyleReply = tags.lastOrNull { it.size > 3 && it[0] == "e" && it[3] == "reply" }?.get(1)
         val newStyleRoot = tags.lastOrNull { it.size > 3 && it[0] == "e" && it[3] == "root" }?.get(1)
 
@@ -80,7 +80,7 @@ open class BaseTextNoteEvent(
     }
 
     fun replyingToAddress(): ATag? {
-        val oldStylePositional = tags.lastOrNull { it.size > 1 && it[0] == "a" }?.let { ATag.parseAtag(it[1], it[2]) }
+        val oldStylePositional = tags.lastOrNull { it.size > 1 && it.size <= 3 && it[0] == "a" }?.let { ATag.parseAtag(it[1], it[2]) }
         val newStyleReply = tags.lastOrNull { it.size > 3 && it[0] == "a" && it[3] == "reply" }?.let { ATag.parseAtag(it[1], it[2]) }
         val newStyleRoot = tags.lastOrNull { it.size > 3 && it[0] == "a" && it[3] == "root" }?.let { ATag.parseAtag(it[1], it[2]) }
 
@@ -88,7 +88,7 @@ open class BaseTextNoteEvent(
     }
 
     fun replyingToAddressOrEvent(): String? {
-        val oldStylePositional = tags.lastOrNull { it.size > 1 && (it[0] == "e" || it[0] == "a") }?.get(1)
+        val oldStylePositional = tags.lastOrNull { it.size > 1 && it.size <= 3 && (it[0] == "e" || it[0] == "a") }?.get(1)
         val newStyleReply = tags.lastOrNull { it.size > 3 && (it[0] == "e" || it[0] == "a") && it[3] == "reply" }?.get(1)
         val newStyleRoot = tags.lastOrNull { it.size > 3 && (it[0] == "e" || it[0] == "a") && it[3] == "root" }?.get(1)
 
