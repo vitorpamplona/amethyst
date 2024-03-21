6f0e4f1f19f0cb7b705706a01a41118be4475380
Vitor Pamplona <vitor@vitorpamplona.com>
2024-03-21 16:55:11 -0400

    Makes hidden cards full width on the discovery feed

--- app/src/main/java/com/vitorpamplona/amethyst/ui/note/ChatroomMessageCompose.kt
+++ app/src/main/java/com/vitorpamplona/amethyst/ui/note/ChatroomMessageCompose.kt
@@ -28,6 +28,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -108,6 +109,7 @@ fun ChatroomMessageCompose(
         WatchBlockAndReport(
             note = baseNote,
             showHiddenWarning = innerQuote,
+            modifier = Modifier.fillMaxWidth(),
             accountViewModel = accountViewModel,
             nav = nav,
         ) { canPreview ->
