25823b1f397f9269e64c8fd0f401fc42a834a30e
VASH <vforvanish@gmail.com>
2024-07-27 13:05:20 +0200

    Fix copying to clipboard edited notes

--- amethyst/src/main/java/com/vitorpamplona/amethyst/ui/note/elements/DropDownMenu.kt
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/ui/note/elements/DropDownMenu.kt
@@ -188,8 +188,9 @@ fun NoteDropDownMenu(
         DropdownMenuItem(
             text = { Text(stringRes(R.string.copy_text)) },
             onClick = {
+                val lastNoteVersion = (editState?.value as? GenericLoadable.Loaded)?.loaded?.modificationToShow?.value ?: note
                 scope.launch(Dispatchers.IO) {
-                    accountViewModel.decrypt(note) { clipboardManager.setText(AnnotatedString(it)) }
+                    accountViewModel.decrypt(lastNoteVersion) { clipboardManager.setText(AnnotatedString(it)) }
                     onDismiss()
                 }
             },
