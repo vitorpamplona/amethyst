d1278a44771b3bf29ef359881b9b183a2758eee7
Vitor Pamplona <vitor@vitorpamplona.com>
2024-07-31 14:16:44 -0400

    Better rendering for live streams that are planned but already finished.

--- amethyst/src/main/java/com/vitorpamplona/amethyst/ui/note/types/LiveActivity.kt
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/ui/note/types/LiveActivity.kt
@@ -21,11 +21,13 @@
 package com.vitorpamplona.amethyst.ui.note.types
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -40,13 +42,19 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.min
+import coil.compose.AsyncImage
 import com.vitorpamplona.amethyst.R
+import com.vitorpamplona.amethyst.model.LocalCache
 import com.vitorpamplona.amethyst.model.Note
 import com.vitorpamplona.amethyst.model.User
 import com.vitorpamplona.amethyst.ui.actions.CrossfadeIfEnabled
 import com.vitorpamplona.amethyst.ui.components.VideoView
+import com.vitorpamplona.amethyst.ui.components.mockAccountViewModel
 import com.vitorpamplona.amethyst.ui.note.ClickableUserPicture
+import com.vitorpamplona.amethyst.ui.note.DisplayAuthorBanner
 import com.vitorpamplona.amethyst.ui.note.UsernameDisplay
 import com.vitorpamplona.amethyst.ui.screen.equalImmutableLists
 import com.vitorpamplona.amethyst.ui.screen.loggedIn.AccountViewModel
@@ -56,12 +64,43 @@ import com.vitorpamplona.amethyst.ui.screen.loggedIn.LiveFlag
 import com.vitorpamplona.amethyst.ui.screen.loggedIn.ScheduledFlag
 import com.vitorpamplona.amethyst.ui.stringRes
 import com.vitorpamplona.amethyst.ui.theme.StdHorzSpacer
+import com.vitorpamplona.amethyst.ui.theme.ThemeComparisonColumn
+import com.vitorpamplona.amethyst.ui.theme.imageModifier
 import com.vitorpamplona.amethyst.ui.theme.placeholderText
+import com.vitorpamplona.quartz.events.Event
 import com.vitorpamplona.quartz.events.LiveActivitiesEvent
 import com.vitorpamplona.quartz.events.Participant
+import com.vitorpamplona.quartz.utils.TimeUtils
 import kotlinx.collections.immutable.ImmutableList
 import kotlinx.collections.immutable.persistentListOf
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withContext
 import java.util.Locale
+
+@Composable
+@Preview
+fun RenderLiveActivityEventPreview() {
+    val event = Event.fromJson("{\"id\":\"19406ad34ce3c653d62eb73c1816ac27dcf473c2ccdccf5af7d90d2633c62561\",\"pubkey\":\"6b66886b3add72c779d205be574ec2d7cec619061ac3e75717389b26445989e4\",\"created_at\":1719084750,\"kind\":30311,\"tags\":[[\"r\",\"podcast:guid:72d5e069-f907-5ee7-b0d7-45404f4f0aa5\"],[\"r\",\"podcast:item:guid:bfc33d6e-e00f-4f11-a2ff-94865b7867aa\"],[\"d\",\"bfc33d6e-e00f-4f11-a2ff-94865b7867aa\"],[\"title\",\"The Online Identity Time Bomb\"],[\"summary\",\"Online identity is a ticking time bomb. But are trustworthy, open-source solutions ready to disarm it, or will we be stuck with lackluster, proprietary systems?\\n Live chat: https:/jblive.tv\"],[\"streaming\",\"https://jblive.fm\"],[\"starts\",\"1719167400\"],[\"status\",\"planned\"],[\"image\",\"https://station.us-iad-1.linodeobjects.com/art/lup-mp3.jpg\"]],\"content\":\"\",\"sig\":\"2ce3fae9ad4512541aaae4dbd9484f50df62ab95ba935d7512b736f087f151c1d15ec2bf62d3135474f16c3e070f335b24349c5493461873ddca3051804ca944\"}") as LiveActivitiesEvent
+    val accountViewModel = mockAccountViewModel()
+    val nav: (String) -> Unit = {}
+    val baseNote: Note?
+
+    runBlocking {
+        withContext(Dispatchers.IO) {
+            LocalCache.justConsume(event, null)
+            baseNote = LocalCache.getOrCreateNote("19406ad34ce3c653d62eb73c1816ac27dcf473c2ccdccf5af7d90d2633c62561")
+        }
+    }
+
+    if (baseNote != null) {
+        ThemeComparisonColumn(
+            toPreview = {
+                RenderLiveActivityEvent(baseNote = baseNote, accountViewModel, nav)
+            },
+        )
+    }
+}
 
 @Composable
 fun RenderLiveActivityEvent(
@@ -129,20 +168,6 @@ fun RenderLiveActivityEventInner(
         }
     }
 
-    var participantUsers by remember {
-        mutableStateOf<ImmutableList<Pair<Participant, User>>>(
-            persistentListOf(),
-        )
-    }
-
-    LaunchedEffect(key1 = eventUpdates) {
-        accountViewModel.loadParticipants(participants) { newParticipantUsers ->
-            if (!equalImmutableLists(newParticipantUsers, participantUsers)) {
-                participantUsers = newParticipantUsers
-            }
-        }
-    }
-
     media?.let { media ->
         if (status == LiveActivitiesEvent.STATUS_LIVE) {
             CheckIfUrlIsOnline(media, accountViewModel) { isOnline ->
@@ -178,19 +203,43 @@ fun RenderLiveActivityEventInner(
                     }
                 }
             }
-        } else if (status == LiveActivitiesEvent.STATUS_ENDED) {
-            Row(
-                verticalAlignment = Alignment.CenterVertically,
-                modifier =
-                    Modifier
-                        .padding(10.dp)
-                        .height(100.dp),
-            ) {
-                Text(
-                    text = stringRes(id = R.string.live_stream_has_ended),
-                    color = MaterialTheme.colorScheme.onBackground,
-                    fontWeight = FontWeight.Bold,
-                )
+        } else {
+            if (status == LiveActivitiesEvent.STATUS_ENDED || (status == LiveActivitiesEvent.STATUS_PLANNED && (starts ?: 0) < TimeUtils.eightHoursAgo())) {
+                Box(
+                    contentAlignment = Alignment.Center,
+                    modifier =
+                        Modifier
+                            .padding(vertical = 10.dp)
+                            .heightIn(min = 100.dp),
+                ) {
+                    cover?.let {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            AsyncImage(model = it, contentDescription = null, modifier = MaterialTheme.colorScheme.imageModifier)
+                        }
+                    } ?: run { DisplayAuthorBanner(baseNote) }
+
+                    Text(
+                        text = stringRes(id = R.string.live_stream_has_ended),
+                        color = MaterialTheme.colorScheme.onBackground,
+                        fontWeight = FontWeight.Bold,
+                    )
+                }
+            }
+        }
+    }
+
+    var participantUsers by remember {
+        mutableStateOf<ImmutableList<Pair<Participant, User>>>(
+            persistentListOf(),
+        )
+    }
+
+    LaunchedEffect(key1 = eventUpdates) {
+        accountViewModel.loadParticipants(participants) { newParticipantUsers ->
+            if (!equalImmutableLists(newParticipantUsers, participantUsers)) {
+                participantUsers = newParticipantUsers
             }
         }
     }
