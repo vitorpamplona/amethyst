255f46404605136f6dd07d6bbf4f98c330683e33
Vitor Pamplona <vitor@vitorpamplona.com>
2024-08-29 10:29:46 -0400

    Basic code for dynamic limits

--- ammolite/src/main/java/com/vitorpamplona/ammolite/relays/filters/Filter.kt
+++ ammolite/src/main/java/com/vitorpamplona/ammolite/relays/filters/Filter.kt
@@ -35,4 +35,15 @@ class Filter(
     fun toJson() = FilterSerializer.toJson(ids, authors, kinds, tags, since, until, limit, search)
 
     fun match(event: Event) = FilterMatcher.match(event, ids, authors, kinds, tags, since, until)
+
+    fun copy(
+        ids: List<String>? = this.ids,
+        authors: List<String>? = this.authors,
+        kinds: List<Int>? = this.kinds,
+        tags: Map<String, List<String>>? = this.tags,
+        since: Long? = this.since,
+        until: Long? = this.until,
+        limit: Int? = this.limit,
+        search: String? = this.search,
+    ) = Filter(ids, authors, kinds, tags, since, until, limit, search)
 }
