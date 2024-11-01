d763f43b40234c22f4e2b91dee1002784963fd8b
KotlinGeekDev <kotlingeek@protonmail.com>
2024-11-01 22:27:55 +0100

    Restore function for getting follow sets(due to merge of upstream).

--- amethyst/src/main/java/com/vitorpamplona/amethyst/model/LocalCache.kt
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/model/LocalCache.kt
@@ -2163,6 +2163,19 @@ object LocalCache {
         }
     }
 
+    fun getFollowSetsFor(user: User): List<AddressableNote> {
+        checkNotInMainThread()
+
+        return addressables
+            .filter { _, note ->
+                val listEvent = note.event
+                (
+                    listEvent is PeopleListEvent &&
+                        user.pubkeyHex == listEvent.pubKey
+                )
+            }
+    }
+
     suspend fun findStatusesForUser(user: User): ImmutableList<AddressableNote> {
         checkNotInMainThread()
 
