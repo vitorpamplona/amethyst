122b926c004dd4dd703da46da3267baa59980e49
greenart7c3 <greenart7c3@proton.me>
2025-05-05 16:33:05 -0300

    Add tip choice popup and setup

--- amethyst/src/main/java/com/vitorpamplona/amethyst/model/Account.kt
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/model/Account.kt
@@ -1198,6 +1198,16 @@ class Account(
         }
     }
 
+    fun updateTipAmounts(amountSet: List<Double>) {
+        var changed = false
+
+        if (settings.changeTipAmounts(amountSet)) changed = true
+
+        if (changed) {
+            sendNewAppSpecificData()
+        }
+    }
+
     fun toggleDontTranslateFrom(languageCode: String) {
         settings.toggleDontTranslateFrom(languageCode)
         sendNewAppSpecificData()
