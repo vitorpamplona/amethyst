7267ecaaf1f39bdf9d9f6762c41f26e85301743d
greenart7c3 <greenart7c3@proton.me>
2025-05-05 16:33:05 -0300

    Add tip choice popup and setup

--- amethyst/src/main/java/com/vitorpamplona/amethyst/ui/note/Icons.kt
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/ui/note/Icons.kt
@@ -50,7 +50,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
