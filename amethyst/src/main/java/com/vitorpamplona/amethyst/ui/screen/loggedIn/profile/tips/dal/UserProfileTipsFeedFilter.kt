e1dc205e9e8d602ee80be6283c49750c741dc047
greenart7c3 <greenart7c3@proton.me>
2025-05-14 08:20:13 -0300

    Add tip tab in the profile screen

---
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/ui/screen/loggedIn/profile/tips/dal/UserProfileTipsFeedFilter.kt
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2024 Vitor Pamplona
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.vitorpamplona.amethyst.ui.screen.loggedIn.profile.tips.dal
+
+import com.vitorpamplona.amethyst.model.Note
+import com.vitorpamplona.amethyst.model.User
+import com.vitorpamplona.amethyst.ui.dal.FeedFilter
+import com.vitorpamplona.amethyst.ui.screen.loggedIn.profile.tips.Tip
+import com.vitorpamplona.quartz.experimental.tipping.TipEvent
+
+class UserProfileTipsFeedFilter(
+    val user: User,
+) : FeedFilter<Tip>() {
+    override fun feedKey(): String = user.pubkeyHex
+
+    override fun feed(): List<Tip> = forProfileFeed(user.tips)
+
+    override fun limit() = 400
+
+    companion object {
+        fun forProfileFeed(tips: List<Note>?): List<Tip> {
+            if (tips == null) return emptyList()
+
+            return (
+                tips
+                    .map { entry -> Tip(entry) }
+                    .sortedBy { (it.tip.event as? TipEvent)?.amount }
+                    .reversed()
+            )
+        }
+    }
+}
