e0449a9d44f421583068a3d28f3bd77dc4c9f115
Vitor Pamplona <vitor@vitorpamplona.com>
2025-05-14 16:52:19 -0400

    Adds a list state saver to the messages screen

--- amethyst/src/main/java/com/vitorpamplona/amethyst/ui/screen/loggedIn/chats/rooms/twopane/ChatroomListPane.kt
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/ui/screen/loggedIn/chats/rooms/twopane/ChatroomListPane.kt
@@ -31,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.unit.dp
 import com.vitorpamplona.amethyst.R
 import com.vitorpamplona.amethyst.ui.feeds.FeedContentState
+import com.vitorpamplona.amethyst.ui.feeds.ScrollStateKeys
 import com.vitorpamplona.amethyst.ui.feeds.WatchLifecycleAndUpdateModel
 import com.vitorpamplona.amethyst.ui.navigation.INav
 import com.vitorpamplona.amethyst.ui.screen.loggedIn.AccountViewModel
@@ -60,8 +61,8 @@ fun ChatroomList(
         remember(knownFeedContentState, markKnownAsRead) {
             derivedStateOf {
                 listOf(
-                    MessagesTabItem(R.string.known, knownFeedContentState, markKnownAsRead),
-                    MessagesTabItem(R.string.new_requests, newFeedContentState, markNewAsRead),
+                    MessagesTabItem(R.string.known, ScrollStateKeys.MESSAGES_KNOWN, knownFeedContentState, markKnownAsRead),
+                    MessagesTabItem(R.string.new_requests, ScrollStateKeys.MESSAGES_NEW, newFeedContentState, markNewAsRead),
                 )
             }
         }
