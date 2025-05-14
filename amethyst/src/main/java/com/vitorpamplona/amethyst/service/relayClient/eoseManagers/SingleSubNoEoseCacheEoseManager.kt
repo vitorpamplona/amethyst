4fc2d3a7d1161fdd97be8d93e42cf484aa2cc26d
Vitor Pamplona <vitor@vitorpamplona.com>
2025-05-14 14:53:54 -0400

    Adds a new EOSE manager that keeps one sub but no EOSE cache

---
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/service/relayClient/eoseManagers/SingleSubNoEoseCacheEoseManager.kt
@@ -0,0 +1,53 @@
+/**
+ * Copyright (c) 2025 Vitor Pamplona
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.vitorpamplona.amethyst.service.relayClient.eoseManagers
+
+import com.vitorpamplona.ammolite.relays.NostrClient
+import com.vitorpamplona.ammolite.relays.TypedFilter
+import kotlin.collections.distinctBy
+
+/**
+ * This query type creates only ONE relay subscription and does not
+ * store any EOSE cache. The EOSE is irrelevant for these types of
+ * filters
+ */
+abstract class SingleSubNoEoseCacheEoseManager<T>(
+    client: NostrClient,
+    allKeys: () -> Set<T>,
+    val invalidateAfterEose: Boolean = false,
+) : BaseEoseManager<T>(client, allKeys) {
+    val sub =
+        orchestrator.requestNewSubscription { time, relayUrl ->
+            if (invalidateAfterEose) {
+                invalidateFilters()
+            }
+        }
+
+    override fun updateSubscriptions(keys: Set<T>) {
+        val uniqueSubscribedAccounts = keys.distinctBy { distinct(it) }
+
+        sub.typedFilters = updateFilter(uniqueSubscribedAccounts)?.ifEmpty { null }
+    }
+
+    abstract fun updateFilter(key: List<T>): List<TypedFilter>?
+
+    abstract fun distinct(key: T): Any
+}
