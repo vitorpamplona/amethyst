f4759c283eb495944b7ce19bc3c2df99ccc6a780
Vitor Pamplona <vitor@vitorpamplona.com>
2025-08-15 14:33:05 -0400

    Adds a basic OkHttp builder to Quartz (new dependency :( ) Adds some instructions to the basic readme.md

---
+++ quartz/src/main/java/com/vitorpamplona/quartz/nip01Core/relay/client/NostrClientSubscription.kt
@@ -0,0 +1,60 @@
+/**
+ * Copyright (c) 2025 Vitor Pamplona
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.vitorpamplona.quartz.nip01Core.relay.client
+
+import com.vitorpamplona.quartz.nip01Core.core.Event
+import com.vitorpamplona.quartz.nip01Core.relay.client.listeners.IRelayClientListener
+import com.vitorpamplona.quartz.nip01Core.relay.client.single.IRelayClient
+import com.vitorpamplona.quartz.nip01Core.relay.filters.Filter
+import com.vitorpamplona.quartz.nip01Core.relay.normalizer.NormalizedRelayUrl
+import com.vitorpamplona.quartz.utils.RandomInstance
+
+class NostrClientSubscription(
+    val client: NostrClient,
+    val filter: () -> Map<NormalizedRelayUrl, List<Filter>> = { emptyMap() },
+    val onEvent: (event: Event) -> Unit = {},
+) : IRelayClientListener {
+    private val subId = RandomInstance.randomChars(10)
+
+    init {
+        client.subscribe(this)
+    }
+
+    override fun onEvent(
+        relay: IRelayClient,
+        subId: String,
+        event: Event,
+        arrivalTime: Long,
+        afterEOSE: Boolean,
+    ) {
+        if (this.subId == subId) {
+            onEvent(event)
+        }
+    }
+
+    /**
+     * Creates or Updates the filter with relays. This method should be called
+     * everytime the filter changes.
+     */
+    fun updateFilter() = client.openReqSubscription(subId, filter())
+
+    fun closeSubscription() = client.close(subId)
+}
