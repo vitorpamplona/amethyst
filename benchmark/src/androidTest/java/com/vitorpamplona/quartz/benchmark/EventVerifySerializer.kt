faffe2a93677174885941caf34bb4bab6b166799
Vitor Pamplona <vitor@vitorpamplona.com>
2025-09-17 08:36:53 -0400

    About 30-40% event hashing performance boost

---
+++ benchmark/src/androidTest/java/com/vitorpamplona/quartz/benchmark/EventVerifySerializer.kt
@@ -0,0 +1,58 @@
+/**
+ * Copyright (c) 2025 Vitor Pamplona
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the
+ * Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN
+ * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+package com.vitorpamplona.quartz.benchmark
+
+import androidx.benchmark.junit4.BenchmarkRule
+import androidx.benchmark.junit4.measureRepeated
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.vitorpamplona.quartz.nip01Core.core.Event
+import com.vitorpamplona.quartz.nip01Core.crypto.EventHasher
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Benchmark, which will execute on an Android device.
+ *
+ * The body of [BenchmarkRule.measureRepeated] is measured in a loop, and Studio will output the
+ * result. Modify your code to see how it affects performance.
+ */
+@RunWith(AndroidJUnit4::class)
+class EventVerifySerializer {
+    @get:Rule
+    val benchmarkRule = BenchmarkRule()
+
+    @Test
+    fun serializeToCheckId() {
+        val event = Event.fromJson(largeKind1Event)
+        benchmarkRule.measureRepeated {
+            EventHasher.makeJsonForId(event.pubKey, event.createdAt, event.kind, event.tags, event.content)
+        }
+    }
+
+    @Test
+    fun fastSerializeToCheckId() {
+        val event = Event.fromJson(largeKind1Event)
+        benchmarkRule.measureRepeated {
+            EventHasher.fastMakeJsonForId(event.pubKey, event.createdAt, event.kind, event.tags, event.content)
+        }
+    }
+}
