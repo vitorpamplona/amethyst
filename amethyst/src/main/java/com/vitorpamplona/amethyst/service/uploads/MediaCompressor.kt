544d6d39335a2333ec037b560c15585355a4a529
davotoula <david.kaspar@gmail.com>
2025-09-23 09:20:41 +0200

    remove plan

--- amethyst/src/main/java/com/vitorpamplona/amethyst/service/uploads/MediaCompressor.kt
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/service/uploads/MediaCompressor.kt
@@ -46,24 +46,6 @@ class MediaCompressorResult(
     val size: Long?,
 )
 
-/** The plan
- * xxx 1. Check input resolution and input fps
- * xxx 2. Create configuration matrix: for each quality level, set bitrate based on input resolution
- * xxx 3. Create Configuration with no quality setting, a bitrate setting, resizer, streamable = true, isMinBitrateCheckEnabled=false
- * xxx 4. Don't upload converted file if compression results in larger file (return MediaCompressorResult(uri, contentType, null))
- * xxx 5. Add toast message about file size saving
- * xxx 6. refactor (helper class for video compression)
- * xxx 7. Fix toast for case when compressed file is larger than original
- * xxx 8. fix ratio multiplier for framerate ->60
- *
- *
- * Don't use Configuration.quality which only determines bitrate. Instead let's create aggressive bitrates based on input and selected quality
- * H265 is not supported, use bitrates that suit h264
- * Detect fps in source, multiple bitrate by 1.5 if 60fps or higher
- * Bitrate floor has to be 1Mbps for now
- * Future extension: Modify library to use bps for bitrate instead of Mbps which allows only 1Mbps as lowest and increments of 1 (Int)
- *
- */
 class MediaCompressor {
     // ALL ERRORS ARE IGNORED. The original file is returned.
     suspend fun compress(
