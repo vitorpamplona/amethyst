7994945209c9080d8f1deaf38771a886d59e22e0
Vitor Pamplona <vitor@vitorpamplona.com>
2025-10-03 15:58:01 -0400

    30% Faster isHex for strings with 64 bytes.

--- quartz/src/commonMain/kotlin/com/vitorpamplona/quartz/utils/Hex.kt
+++ quartz/src/commonMain/kotlin/com/vitorpamplona/quartz/utils/Hex.kt
@@ -36,22 +36,113 @@ object Hex {
             (LOWER_CASE_HEX[(it shr 4)].code shl 8) or LOWER_CASE_HEX[(it and 0xF)].code
         }
 
+    // 47ns in debug on the Emulator
     fun isHex(hex: String?): Boolean {
-        if (hex.isNullOrEmpty()) return false
+        if (hex == null) return false
         if (hex.length and 1 != 0) return false
 
-        try {
-            for (c in hex.indices) {
-                if (c < 0 || c > 255) return false
-                if (hexToByte[hex[c].code] < 0) return false
-            }
+        return try {
+            internalIsHex(hex, hexToByte)
         } catch (_: IllegalArgumentException) {
             // there are p tags with emoji's which makes the hex[c].code > 256
-            return false
+            false
+        } catch (_: IndexOutOfBoundsException) {
+            // there are p tags with emoji's which makes the hex[c].code > 256
+            false
         }
+    }
 
+    // breaking this function away from the main one improves performance for some reason
+    fun internalIsHex(
+        hex: String,
+        hexToByte: IntArray,
+    ): Boolean {
+        for (c in hex.indices) {
+            if (hexToByte[hex[c].code] < 0) return false
+        }
         return true
     }
+
+    // 30% faster than isHex
+    fun isHex64(hex: String): Boolean =
+        try {
+            hexToByte[hex[0].code] >= 0 &&
+                hexToByte[hex[1].code] >= 0 &&
+                hexToByte[hex[2].code] >= 0 &&
+                hexToByte[hex[3].code] >= 0 &&
+                hexToByte[hex[4].code] >= 0 &&
+                hexToByte[hex[5].code] >= 0 &&
+                hexToByte[hex[6].code] >= 0 &&
+                hexToByte[hex[7].code] >= 0 &&
+                hexToByte[hex[8].code] >= 0 &&
+                hexToByte[hex[9].code] >= 0 &&
+
+                hexToByte[hex[10].code] >= 0 &&
+                hexToByte[hex[11].code] >= 0 &&
+                hexToByte[hex[12].code] >= 0 &&
+                hexToByte[hex[13].code] >= 0 &&
+                hexToByte[hex[14].code] >= 0 &&
+                hexToByte[hex[15].code] >= 0 &&
+                hexToByte[hex[16].code] >= 0 &&
+                hexToByte[hex[17].code] >= 0 &&
+                hexToByte[hex[18].code] >= 0 &&
+                hexToByte[hex[19].code] >= 0 &&
+
+                hexToByte[hex[20].code] >= 0 &&
+                hexToByte[hex[21].code] >= 0 &&
+                hexToByte[hex[22].code] >= 0 &&
+                hexToByte[hex[23].code] >= 0 &&
+                hexToByte[hex[24].code] >= 0 &&
+                hexToByte[hex[25].code] >= 0 &&
+                hexToByte[hex[26].code] >= 0 &&
+                hexToByte[hex[27].code] >= 0 &&
+                hexToByte[hex[28].code] >= 0 &&
+                hexToByte[hex[29].code] >= 0 &&
+
+                hexToByte[hex[30].code] >= 0 &&
+                hexToByte[hex[31].code] >= 0 &&
+                hexToByte[hex[32].code] >= 0 &&
+                hexToByte[hex[33].code] >= 0 &&
+                hexToByte[hex[34].code] >= 0 &&
+                hexToByte[hex[35].code] >= 0 &&
+                hexToByte[hex[36].code] >= 0 &&
+                hexToByte[hex[37].code] >= 0 &&
+                hexToByte[hex[38].code] >= 0 &&
+                hexToByte[hex[39].code] >= 0 &&
+
+                hexToByte[hex[40].code] >= 0 &&
+                hexToByte[hex[41].code] >= 0 &&
+                hexToByte[hex[42].code] >= 0 &&
+                hexToByte[hex[43].code] >= 0 &&
+                hexToByte[hex[44].code] >= 0 &&
+                hexToByte[hex[45].code] >= 0 &&
+                hexToByte[hex[46].code] >= 0 &&
+                hexToByte[hex[47].code] >= 0 &&
+                hexToByte[hex[48].code] >= 0 &&
+                hexToByte[hex[49].code] >= 0 &&
+
+                hexToByte[hex[50].code] >= 0 &&
+                hexToByte[hex[51].code] >= 0 &&
+                hexToByte[hex[52].code] >= 0 &&
+                hexToByte[hex[53].code] >= 0 &&
+                hexToByte[hex[54].code] >= 0 &&
+                hexToByte[hex[55].code] >= 0 &&
+                hexToByte[hex[56].code] >= 0 &&
+                hexToByte[hex[57].code] >= 0 &&
+                hexToByte[hex[58].code] >= 0 &&
+                hexToByte[hex[59].code] >= 0 &&
+
+                hexToByte[hex[60].code] >= 0 &&
+                hexToByte[hex[61].code] >= 0 &&
+                hexToByte[hex[62].code] >= 0 &&
+                hexToByte[hex[63].code] >= 0
+        } catch (_: IllegalArgumentException) {
+            // there are p tags with emoji's which makes the hex[c].code > 256
+            false
+        } catch (_: IndexOutOfBoundsException) {
+            // there are p tags with emoji's which makes the hex[c].code > 256
+            false
+        }
 
     fun decode(hex: String): ByteArray {
         // faster version of hex decoder
