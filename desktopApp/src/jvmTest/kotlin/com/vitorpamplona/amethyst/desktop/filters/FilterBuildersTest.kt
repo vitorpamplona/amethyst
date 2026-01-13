04ac40fa1ae0dfc9bb2323b0c8d85be8085efca8
Vitor Pamplona <vitor@vitorpamplona.com>
2026-01-13 16:07:03 -0500

    moves test as well

--- desktopApp/src/jvmTest/kotlin/com/vitorpamplona/amethyst/desktop/filters/FilterBuildersTest.kt
+++ desktopApp/src/jvmTest/kotlin/com/vitorpamplona/amethyst/desktop/filters/FilterBuildersTest.kt
@@ -18,10 +18,10 @@
  * AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
  * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-package com.vitorpamplona.amethyst.commons.filters
+package com.vitorpamplona.amethyst.desktop.filters
 
-import com.vitorpamplona.amethyst.commons.subscriptions.FilterBuilders
-import com.vitorpamplona.amethyst.commons.subscriptions.buildFilter
+import com.vitorpamplona.amethyst.desktop.subscriptions.FilterBuilders
+import com.vitorpamplona.amethyst.desktop.subscriptions.buildFilter
 import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
