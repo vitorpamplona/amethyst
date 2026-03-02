69e061e4c451f7a4cbfc33e7ab5a2b15c80ecb37
Vitor Pamplona <vitor@vitorpamplona.com>
2026-03-01 19:15:36 -0500

    Fixes Stability of the Update Reaction view model

--- amethyst/src/main/java/com/vitorpamplona/amethyst/ui/note/UpdateReactionTypeDialog.kt
+++ amethyst/src/main/java/com/vitorpamplona/amethyst/ui/note/UpdateReactionTypeDialog.kt
@@ -54,6 +54,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.Stable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -99,6 +100,7 @@ import kotlinx.collections.immutable.persistentListOf
 import kotlinx.collections.immutable.toImmutableList
 import kotlinx.coroutines.launch
 
+@Stable
 class UpdateReactionTypeViewModel : ViewModel() {
     lateinit var accountViewModel: AccountViewModel
     lateinit var account: Account
