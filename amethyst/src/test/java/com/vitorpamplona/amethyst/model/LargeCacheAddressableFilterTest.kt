00b06a0e2a78687d0e1917dccb1036991b3025ec
nrobi144 <robert.nagy@swanbitcoin.com>
2026-03-24 09:09:29 +0200

    refactor(cache): move LargeSoftCache to commons/jvmAndroid, fix ICacheProvider types
    - Move LargeSoftCache from amethyst/model to commons/jvmAndroid/model/cache
      so both Android and Desktop share the same WeakReference cache implementation
    - Change ICacheProvider return types from Any? to proper types (User?, Note?)
      since these model classes already live in commons
    - Remove unnecessary casts in ThreadAssembler, ChatNewMessageState, SearchBarState
    - Improve LargeSoftCache.cleanUp() to use single-pass iterator (zero allocation)
    - Update Android imports in LocalCache, LargeSoftCacheAddressExt, and test
    
    Per Vitor's feedback on PR #1905: BoundedLargeCache evicts arbitrarily.
    LargeSoftCache uses WeakReferences so GC respects the reference graph.

--- amethyst/src/test/java/com/vitorpamplona/amethyst/model/LargeCacheAddressableFilterTest.kt
+++ amethyst/src/test/java/com/vitorpamplona/amethyst/model/LargeCacheAddressableFilterTest.kt
@@ -20,6 +20,7 @@
  */
 package com.vitorpamplona.amethyst.model
 
+import com.vitorpamplona.amethyst.commons.model.cache.LargeSoftCache
 import com.vitorpamplona.quartz.nip01Core.core.Address
 import com.vitorpamplona.quartz.nip01Core.core.HexKey
 import junit.framework.TestCase.assertEquals
