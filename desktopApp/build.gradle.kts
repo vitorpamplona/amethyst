e90945d201364907d4793a1dfe6cb7260f4413fd
Vitor Pamplona <vitor@vitorpamplona.com>
2026-03-25 15:34:32 -0400

    Removing the solution to a warning about spotless dependencies because claude can't run the vlc step

--- desktopApp/build.gradle.kts
+++ desktopApp/build.gradle.kts
@@ -115,7 +115,3 @@ vlcSetup {
     pathToCopyVlcMacosFilesTo.set(file("src/jvmMain/appResources/macos/vlc"))
     pathToCopyVlcWindowsFilesTo.set(file("src/jvmMain/appResources/windows/vlc"))
 }
-
-tasks.named("spotlessKotlin") {
-    inputs.files(tasks.named("vlcSetup"))
-}
